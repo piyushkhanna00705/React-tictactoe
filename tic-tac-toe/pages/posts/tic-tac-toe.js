ba67076ba61fdbc143c7c944d5436a8ef52f01e6
Piyush Khanna <piyushkhanna00705@gmail.com>
2023-06-05 01:30:42 +0530

    Adding time travel to go bac to previous moves

--- tic-tac-toe/pages/posts/tic-tac-toe.js
+++ tic-tac-toe/pages/posts/tic-tac-toe.js
@@ -1,10 +1,57 @@
 import { useState } from "react";
 import styles from  './tic-tac-toe.module.css'
 
-export default function Board() {
-
-    const [squares, setSquares] = useState(Array(9).fill(null));
+export default function Game() {
     const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const currentSquares = history[currentMove];
+
+    function handlePlay(nextSquares) {
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length-1);
+        setXIsNext(!xIsNext);
+    }
+
+    function jumpTo(nextMove) {
+        setCurrentMove(nextMove);
+        setXIsNext(nextMove % 2 ===0);
+    }
+
+    const moves = history.map((squares, move) => {
+
+        let description;
+        if(move > 0) {
+            description = "Go to move #" + move;
+        } else {
+            description = "Go to game start";
+        }
+
+        return (
+            <li key = {move}>
+                <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+        )
+
+
+    });
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board xIsNext = {xIsNext} squares = {currentSquares} onPlay = {handlePlay} />
+            </div>
+            <div className="game-info">
+                <ol>
+                    {moves}
+                </ol>
+            </div>
+        </div>
+    );
+}
+
+function Board({xIsNext, squares, onPlay}) {
     function handleClick (i) {
         if(squares[i] || calculateWinner(squares)) {
             return;
@@ -15,8 +62,9 @@ export default function Board() {
         } else {
             nextSquares[i]='O';
         }
-        setXIsNext(!xIsNext);
-        setSquares(nextSquares);
+        onPlay(nextSquares);
+        // setXIsNext(!xIsNext);
+        // setSquares(nextSquares);
     }
 
     //Declaring Winner
